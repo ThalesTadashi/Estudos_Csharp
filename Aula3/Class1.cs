0b5d3a7d53dc613777a8dc2edf7134cf28cc7278
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-08-19 20:53:20 -0300

    Exemplos de operadores

---
+++ Aula3/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula3
+{
+    class Class1
+    {
+
+        static void main()
+        {
+            int res = (10*5);
+
+            bool res1 = 10 > 5;
+
+            int num = 10;
+
+            //num++;        num += 1;        num = num + 1;
+            //mum+=10;      num += 10;
+            //num*=2;  
+
+            // & = AND / E
+            // | = OR / OU
+
+            bool res2 = (5 > 7) | (10 < 5);
+            bool res3 = (5 > 7) & (10 < 5);
+
+
+            Console.WriteLine(res);
+            Console.WriteLine(res1);
+        }
+    }
+}
