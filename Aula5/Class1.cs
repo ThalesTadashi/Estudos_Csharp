9a27c24e045b2057eeb1c37745fad82ea638c8c2
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-08-23 21:18:19 -0300

    Constantes

---
+++ Aula5/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula5
+{
+    class Class1
+    {
+
+        static void Main()
+        {
+            const string canal = "CFB Cursos";
+            const double pi = 3.1415;
+
+            Console.WriteLine("Canal {0}", canal);
+        }
+    }
+}
