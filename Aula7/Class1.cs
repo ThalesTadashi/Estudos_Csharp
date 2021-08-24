cab44bd12083f76306bb358d4b10f180604018bd
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-08-24 20:03:56 -0300

    Bitwise

---
+++ Aula7/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula7
+{
+    class Class1
+    {
+        static void Main()
+        {
+            int num = 20;
+
+            num = num << 2; //dobre
+            num = num >> 2; //metade
+
+            Console.WriteLine(num);
+        }
+    }
+}
