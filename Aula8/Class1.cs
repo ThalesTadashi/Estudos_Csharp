5463ccdbfec7f9729535968c9a8bcea354930349
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-08-24 20:19:12 -0300

    Enumeradores

---
+++ Aula8/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula8
+{
+    class Class1
+    {
+        enum DiasSemana { Domingo,Segunda,Terça,Quarta,Quinta,Sexta,Sábado};
+
+        static void Main()
+        {
+            //DiasSemana ds = DiasSemana.Domingo;
+
+            //              ou
+
+            //DiasSemana ds = (DiasSemana)0;
+
+            //        Também podemos:
+
+            int ds = (int)DiasSemana.Sexta;
+
+            Console.WriteLine(ds);
+        }
+    }
+}
