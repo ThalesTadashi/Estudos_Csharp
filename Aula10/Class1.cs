16244d914ae37415ae7bbae94aadfb82b4526a91
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-08-25 19:46:35 -0300

    If

---
+++ Aula10/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula10
+{
+    class Class1
+    {
+
+        static void Main()
+        {
+            //SE(Espressão Lógica){}
+
+            int nota = 80;
+            string resultado = "Reprovado";
+
+            if (nota>=60)
+            {
+                resultado = "Aprovado";
+            }
+            Console.WriteLine("Resultado: {0}",resultado);
+        }
+
+    }
+}
