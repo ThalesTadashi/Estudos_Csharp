7ddce3e63382a8d48894518261986147d3b05fab
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-08-26 19:52:50 -0300

    If e Else

---
+++ Aula11/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula11
+{
+    class Class1
+    {
+        static void Main()
+        {
+            //SE(Espressão Lógica){}
+
+            int nota = 80;
+            string resultado;
+
+
+            // >= 60 - Aprovado
+            // 59 e 40 - Recuperação
+            // <40 - Reprovado
+
+            if (nota < 40)
+            {
+                resultado = "Reprovado";
+            }
+            else if (nota <60)
+            {
+                resultado = "Recuperação";
+            }
+            else
+            {
+                resultado = "Aprovado";
+            }
+            Console.WriteLine("Resultado: {0}", resultado);
+        }
+    }
+}
