39d399e389a61a57bd0931ca7394ce34ca4d000c
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-08-30 22:50:36 -0300

    Switch

---
+++ Aula13/Class1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula13
+{
+    class Class1
+    {
+
+        static void Main()
+        {
+            int tempo = 0;
+            char escolha;
+
+            Console.WriteLine("Belo Horizonte/MG a Vìtoria/ES");
+            Console.WriteLine("Escolha o Transporte: [a]=Avião | [c]=Carro | [o]=Ônibus ");
+
+            escolha = char.Parse(Console.ReadLine());
+
+            switch (escolha)
+            {
+                case 'a':
+                case 'A':
+                    tempo = 50;
+                    break;
+
+                case 'c':
+                case 'C':
+                    tempo = 480;
+                    break;
+
+                case 'o':
+                case 'O':
+                    tempo = 660;
+                    break;
+
+                default:
+                    tempo = -1;
+                    break;
+
+            }
+            if (tempo<0)
+            {
+                Console.WriteLine("Trasnporte Inisponível");
+            }
+            else
+            {
+                Console.WriteLine("Para o Trasporte escolhido o temmpo é: {0} Minutos", tempo);
+            }
+        }
+
+    }
+}
