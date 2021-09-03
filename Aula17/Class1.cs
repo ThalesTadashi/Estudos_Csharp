a46fd3f983ff4207942333df2186a2e997c02846
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-03 07:58:41 -0300

    Loop For

---
+++ Aula17/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula17
+{
+    class Class1
+    {
+        static void Main()
+        {
+            int[] num = new int[10];
+
+            for (int i=0;i<num.Length;i++)
+            {
+                num[i] = 0;
+                Console.WriteLine("O valor de num na posição {0} é: {1}", i, num[i]) ;
+            }
+   
+        }
+    }
+}
