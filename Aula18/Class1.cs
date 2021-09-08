b156388b3ec186008c50851d04fd0ef9eac11c47
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-08 20:15:18 -0300

    Loop While

---
+++ Aula18/Class1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula18
+{
+    class Class1
+    {
+        static void Main()
+        {
+            int[] num = new int[10];
+            int i = 0;
+
+            while (i < num.Length)
+            {
+                num[i]= 0;
+                Console.WriteLine(num[i]);
+                i++;
+            }
+
+            /*while (i<10)
+            {
+                Console.WriteLine("Contagem: {0}",i);
+                i++;
+            }
+            */
+            Console.WriteLine("Fim do loop");
+        }
+    }
+}
