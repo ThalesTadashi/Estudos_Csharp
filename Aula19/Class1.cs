527093f3023c18febdc47e5c4573db6210d52c7c
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-09 07:40:38 -0300

    Do While

---
+++ Aula19/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula19
+{
+    class Class1
+    {
+        static void Main()
+        {
+            int num = 5;
+            string senha="123";
+            string senhaUser;
+            int tentativas=0;
+
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Digite a senha");
+                senhaUser = Console.ReadLine();
+                tentativas++;
+            } while (senha != senhaUser);
+
+            Console.Clear();
+            Console.WriteLine("Senha Correta, Você teve {0} tentativas de Acerto",tentativas);
+        }
+    }
+}
