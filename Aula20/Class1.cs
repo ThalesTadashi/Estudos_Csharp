7a213428b350fde3f80adb7b1ed3666d6fae483e
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-10 13:05:20 -0300

    Foreach

---
+++ Aula20/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula20
+{
+    class Class1
+    {
+        static void Main()
+        {
+            int[] num = new int[3] {11,22,33};
+
+            for (int i = 0; i < num.Length; i++)
+            {
+                Console.WriteLine(num[i]);
+            }
+
+
+
+            foreach (int n in num)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
