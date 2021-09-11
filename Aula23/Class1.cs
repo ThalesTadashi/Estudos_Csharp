2c7b783979ba5cbb0eaea92a9d29f6fd8aeb9515
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-11 17:15:40 -0300

    Passagem por valor e referência

---
+++ Aula23/Class1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula23
+{
+    class Class1
+    {
+        static void Main()
+        {
+            int num = 10;
+            dobrar1(ref num);
+            Console.WriteLine(num);
+        }
+
+
+
+        static void dobrar(int valor)
+        {
+            valor*=2;
+        }
+
+        static void dobrar1(ref int valor)
+        {
+            valor *= 2;
+        }
+    }
+}
