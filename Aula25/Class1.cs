158e94bbace80332aa1c0cf447dfa2e44c0cd8c1
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-14 07:44:57 -0300

    Argumento Params

---
+++ Aula25/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula25
+{
+    class Class1
+    {
+        static void Main()
+        {
+            soma();
+        }
+
+        static void soma(params int[]n)
+        {
+            int res=0;
+
+            if (n.Length < 1)
+            {
+                Console.WriteLine("Não temm valores para serem somados");
+            }else if (n.Length <2)
+            {
+                Console.WriteLine("Valores insuficientes para a soma:{0}",n[0]);
+            }
+            else
+            {
+                for (int i = 0; i < n.Length; i++)
+                {
+                    res += n[i];
+                }
+            }
+
+            Console.WriteLine("A soma dos valores é", res);
+        }
+    }
+}
