f66195e618bfbd9837843e5479acf7e200774317
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-17 16:56:29 -0300

    Sobrecarga de Construtores

---
+++ Aula28/Class1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula28
+{
+    public class Jogador
+    {
+        public int energia;
+        public bool vivo;
+        public string nome;
+
+        public Jogador()
+        {
+            energia = 0;
+            vivo = true;
+            nome = "Player";
+        }
+        public Jogador(string n)
+        {
+            energia = 100;
+            vivo = true;
+            nome = n;
+        }
+        public Jogador(string n, int e)
+        {
+            energia = e;
+            vivo = true;
+            nome = n;
+        }
+        public Jogador(string n, int e, bool v)
+        {
+            energia = e;
+            vivo = v;
+            nome = n;
+        }
+        public void Info()
+        {
+            Console.WriteLine("Nome jogador:{0}",nome);
+            Console.WriteLine("Eneergia jogador:{0}", energia);
+            Console.WriteLine("Status do jogador:{0}", vivo);
+        }
+
+    }
+
+    class Class1
+    {
+        public static void Main()
+        {
+            Jogador j1 = new Jogador();
+            Jogador j2 = new Jogador("Lucas");
+            Jogador j3 = new Jogador("Lucas",85);
+            Jogador j4 = new Jogador("gustavo", 0, false);
+
+
+
+            j1.Info();
+            j2.Info();
+            j3.Info();
+            j4.Info();
+
+        }
+
+
+    }
+}
