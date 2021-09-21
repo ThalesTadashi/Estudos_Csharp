f2f4c30f473d2a7591be5acde316072e3253afdd
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-21 14:00:39 -0300

    This

---
+++ Aula30/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula30
+{
+    class Calculos
+    {
+        public int v1;
+        public int v2;
+
+        public Calculos(int v1, int v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+        public int Somar()
+        {
+            return v1 + v2;
+        }
+    }
+    class Class1
+    {
+        static void Main()
+        {
+            Calculos c = new Calculos(10,2);
+
+            Console.WriteLine(c.Somar());
+        }
+    }
+}
