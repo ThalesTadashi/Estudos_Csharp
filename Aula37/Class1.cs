4db23290ed2a0cc37c4ac7230118d9f97a6e6c15
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-09-30 21:59:19 -0300

    Classes e Métodos abstratos

---
+++ Aula37/Class1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula37
+{
+
+    abstract class Veiculo 
+    {
+        protected int velMaxima;
+        protected int velAtual;
+        protected bool ligado;
+       
+        public Veiculo()
+        {
+            ligado = false;
+            velAtual = 0;
+
+        }
+
+        public void setLigado(bool ligado)
+        {
+            this.ligado = ligado;
+        }
+        public int getVelAtual()
+        {
+            return velAtual;
+        }
+
+        abstract public void aceleracao(int mult);
+     
+    }
+
+    class Carro : Veiculo
+    {
+        public Carro()
+        {
+            velMaxima = 120;
+        }
+
+        override public void aceleracao(int mult)
+        {
+            velAtual+=10*mult;
+        }
+    }
+    class Class1
+    {
+        public void Main()
+        {
+
+            Carro c = new Carro();
+
+            c.aceleracao(1);
+
+            Console.WriteLine(c.getVelAtual());
+        }
+    }
+}
