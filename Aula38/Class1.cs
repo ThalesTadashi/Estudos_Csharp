4a91ca3f9da391271f5e92579ff78699e7f9b97a
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-10-01 14:49:33 -0300

    Classe Sealed

---
+++ Aula38/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula38
+{
+
+    sealed class Veiculo
+    {
+
+    }
+    //class Carro : Veiculo //Classe sealed não permite derivar 
+    //{
+    //}
+
+    class Class1
+    {
+        public void Main()
+        {
+
+        }
+    }
+}
