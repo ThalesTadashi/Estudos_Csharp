89a5e836d9cb170afa18e9faa5fc97440bf2a399
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-10-04 23:01:03 -0300

    Acessors GET e SET

---
+++ Aula39/Class1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula39
+{
+
+    class Carro
+    {
+        private int velMax;
+
+        public int vm
+        {
+            get
+            {
+                return velMax;
+            }
+            set
+            {
+                if(value < 0)
+                {
+                    velMax = 0;
+
+                }
+                else if (value>300)
+                {
+                    velMax = 300;
+                }
+                else
+                {
+                    velMax = value;
+                }
+            }
+        }
+
+        public Carro()
+        {
+            vm = 120;
+        }
+
+
+    }
+    class Class1
+    {
+        public void Main()
+        {
+            Carro c1 = new Carro();
+
+            Console.WriteLine("Velocidade: {0}",c1.vm);
+        }
+    }
+}
