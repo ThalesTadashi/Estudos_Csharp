ba6a716df57c05d6363e226b11b7dc0db69a58fa
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-10-07 19:03:42 -0300

    Interfaces

--- Aula41/Class1.cs
+++ Aula41/Class1.cs
@@ -6,7 +6,54 @@ using System.Threading.Tasks;
 
 namespace Aula41
 {
+
+    public interface Veiculo
+    {
+        void ligar();
+        void desligar();
+        void info();
+    }
+    public interface Combate
+    {
+        void disparar();
+    }
+    class Carro:Veiculo,Combate
+    {
+        public bool ligado;
+        private int munição;
+        public Carro()
+        {
+            setMunicao(100);
+        }
+        public void setMunicao(int qtde)
+        {
+            this.munição = qtde;
+        }
+        public void ligar()
+        {
+            this.ligado = true;
+        }
+        public void desligar()
+        {
+            this.ligado = false;
+        }
+        public void info()
+        {
+
+        }
+        public void disparar()
+        {
+            
+        }
+    }
+
     class Class1
     {
+        static void Main()
+        {
+            Carro c1 = new Carro();
+
+            
+        }
     }
 }
