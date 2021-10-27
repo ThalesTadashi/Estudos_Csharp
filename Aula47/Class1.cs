e10cdde00fab935bf0e8faebc37f0d795e9d4f3e
Thales Tadashi Umekawa <thales.ume@gmail.com>
2021-10-27 14:36:47 -0300

    Métodos e Variáveis estáticos

---
+++ Aula47/Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula47
+{
+    class Mat {
+        public static double pi = 3.14;
+
+        public static int dobro(int n)
+        {
+            return n * 2;
+        }
+
+    }
+
+    class Class1
+    {
+        static void Main()
+        {
+            double vpi = Mat.pi;
+            int num = 10;
+            Console.WriteLine(vpi);
+            Console.WriteLine(Mat.dobro(num));
+        }
+    }
+}
